eb49f59147a0cfa8f08c9d78647528e8b0bda9f8
Chris Rinaldi <cgrinaldi@gmail.com>
2015-04-03 14:46:32 -0700

    (feat) Add linting and watch gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 
-// create a default task and just log a message
-gulp.task('default', function() {
-  return gutil.log('Gulp is running!');
+// loading packages
+var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
+
+// configure the jshint task
+gulp.task('jshint', function() {
+  return gulp.src('app/src/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
 });
+
+// configure which files to watch and what tasks to run on file changes
+gulp.task('watch', function() {
+  gulp.watch('app/src/**/*.js', ['jshint']);
+});
+
+// define the default task and add watch task to it
+gulp.task('default', ['jshint', 'watch']);
